dcb95396af2cf73ea2cecde53589f466257295f4
Daniel <danielgf1620@gmail.com>
2026-05-21 14:47:03 -0300

    Adicionando bibliotecas do Supabase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,5 +58,6 @@ dependencies {
     implementation("io.github.jan-tennert.supabase:postgrest-kt:2.0.4")
     implementation("io.github.jan-tennert.supabase:gotrue-kt:2.0.4")
     implementation("io.github.jan-tennert.supabase:storage-kt:2.0.4")
+
     implementation("io.ktor:ktor-client-android:2.3.4")
 }
