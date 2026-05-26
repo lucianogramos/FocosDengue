0fbf9a64e0a5ceab6fd231d84b4d9affcf2f6fcc
Daniel <danielgf1620@gmail.com>
2026-05-26 20:23:54 -0300

    Melhorias nas funções de cadastro e login

--- app/src/main/java/com/foco_acessibilidade_dengue/data/remote/auth/Auth.kt
+++ app/src/main/java/com/foco_acessibilidade_dengue/data/remote/auth/Auth.kt
@@ -4,18 +4,53 @@ import com.foco_acessibilidade_dengue.data.remote.supabase
 import io.github.jan.supabase.gotrue.auth
 import io.github.jan.supabase.gotrue.providers.builtin.Email
 
-suspend fun cadastrarUsuario(email: String, senha: String) {
-    supabase.auth.signUpWith(Email) {
-        this.email = email
-        this.password = senha
+suspend fun cadastrarUsuario(email: String, senha: String): Result<Unit> {
+
+    if (email.isBlank()) {
+        return Result.failure(Exception("Digite um e-mail!"))
     }
+    if (senha.isBlank()) {
+        return Result.failure(Exception("Digite uma senha!"))
+    }
+    if (senha.length < 6) {
+        return Result.failure(Exception("A senha deve conter pelo menos 6 caracteres"))
+    }
+
+    return try {
+        supabase.auth.signUpWith(Email) {
+            this.email = email
+            this.password = senha
+        }
+
+        Result.success(Unit)
+
+    } catch (e: Exception) {
+        Result.failure(e)
+    }
+
 }
 
-suspend fun loginUsuario(email: String, senha: String) {
-    supabase.auth.signInWith(Email) {
-        this.email = email
-        this.password = senha
+suspend fun loginUsuario(email: String, senha: String): Result<Unit> {
+
+    if (email.isBlank()) {
+        return Result.failure(Exception("Digite um e-mail!"))
     }
+    if (senha.isBlank()) {
+        return Result.failure(Exception("Digite uma senha!"))
+    }
+
+    return try {
+        supabase.auth.signInWith(Email) {
+            this.email = email
+            this.password = senha
+        }
+
+        Result.success(Unit)
+
+    } catch (e: Exception) {
+        Result.failure(e)
+    }
+
 }
 
 suspend fun logoutUsuario() {
--- app/src/main/java/com/foco_acessibilidade_dengue/ui/auth_screen/signup/SignUpScreenContent.kt
+++ app/src/main/java/com/foco_acessibilidade_dengue/ui/auth_screen/signup/SignUpScreenContent.kt
@@ -1,5 +1,6 @@
 package com.foco_acessibilidade_dengue.ui.auth_screen.signup
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,8 +18,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -26,14 +29,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.foco_acessibilidade_dengue.R
+import com.foco_acessibilidade_dengue.data.remote.auth.cadastrarUsuario
 import com.foco_acessibilidade_dengue.ui.auth_screen.AuthInput
 import com.foco_acessibilidade_dengue.ui.auth_screen.ClickHereLink
+import kotlinx.coroutines.launch
 
 @Composable
 fun SignUpScreenContent(modifier: Modifier = Modifier, toLoginScreen: (() -> Unit)? = null) {
     var name by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
 
     Column(modifier = modifier.fillMaxSize().padding(24.dp)) {
         Spacer(Modifier.height(4.dp))
@@ -90,7 +98,17 @@ fun SignUpScreenContent(modifier: Modifier = Modifier, toLoginScreen: (() -> Uni
         Spacer(Modifier.height(32.dp))
 
         Button(
-            {},
+            {
+                scope.launch {
+                    val resultado = cadastrarUsuario(email = email, senha = password)
+
+                    resultado.onSuccess {
+                        Toast.makeText(context, "Cadastro realizado!",Toast.LENGTH_LONG).show()
+                    }.onFailure {
+                        Toast.makeText(context, it.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            },
             Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(8.dp),
             colors = ButtonColors(colorScheme.primary, colorScheme.onPrimary, colorScheme.secondary, colorScheme.tertiary)
