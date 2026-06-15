bbfe041e5158f192de1e0adcc5689c80e9e5f4d4
Miguel <miguel.anderson.albino@gmail.com>
2026-06-15 16:37:55 -0300

    AvifKit e conversão implementados.

--- app/src/main/java/com/focos_dengue/data/service/AvifCompService.kt
+++ app/src/main/java/com/focos_dengue/data/service/AvifCompService.kt
@@ -1,13 +1,32 @@
 package com.focos_dengue.data.service
 
+import com.alfikri.rizky.avifkit.AvifConverter
+import com.alfikri.rizky.avifkit.ImageInput
+import com.alfikri.rizky.avifkit.Priority
 import com.focos_dengue.domain.service.ImageCompService
+import java.io.File
 
-class AvifCompressionService : ImageCompService {
+class AvifCompService : ImageCompService {
 
     override suspend fun compressToAvif(
         localPath: String
     ): String {
 
-        return localPath
+        val inputFile = File(localPath)
+
+        val outputPath = File(
+            inputFile.parent,
+            "${inputFile.nameWithoutExtension}.avif"
+        ).absolutePath
+
+        val converter = AvifConverter()
+
+        converter.convertToFile(
+            input = ImageInput.from(localPath),
+            outputPath = outputPath,
+            priority = Priority.BALANCED
+        )
+
+        return outputPath
     }
 }
