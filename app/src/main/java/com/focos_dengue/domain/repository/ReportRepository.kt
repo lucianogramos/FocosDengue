a0cb81222cb6e2656e8a67f78f7222cfe60aa88c
Miguel <miguel.anderson.albino@gmail.com>
2026-06-16 18:48:45 -0300

    Imagem do app, contrato do histórico e usecase

--- app/src/main/java/com/focos_dengue/domain/repository/ReportRepository.kt
+++ app/src/main/java/com/focos_dengue/domain/repository/ReportRepository.kt
@@ -3,5 +3,8 @@ package com.focos_dengue.domain.repository
 import com.focos_dengue.domain.model.Report
 
 interface ReportRepository {
+
     suspend fun submitReport(report: Report): Result<Unit>
+
+    suspend fun getReportHistory(): Result<List<Report>>
 }
